6cefa6011c0b6e8077ff90114834d69998e89b11
BOWENmeZHENG <bowenzheng@lbl.gov>
2026-06-18 14:30:47 -0700

    deleted unused clients.py

--- app/modules/term_extractor/__init__.py
+++ app/modules/term_extractor/__init__.py
@@ -1,14 +1,9 @@
 from .academy_agent import TermExtractorAgent
-from .clients import CBorgChatClient, ChatClient, OllamaChatClient, make_chat_client
 from .orchestrator import Orchestrator, run_extraction
 from .schema import SchemaHelper
 
 __all__ = [
     "TermExtractorAgent",
-    "ChatClient",
-    "OllamaChatClient",
-    "CBorgChatClient",
-    "make_chat_client",
     "Orchestrator",
     "run_extraction",
     "SchemaHelper",
